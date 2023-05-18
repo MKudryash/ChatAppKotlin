67c2148021b5aad2807ad2cd22d1565b3b3a9032
MKudryash <82795441+MKudryash@users.noreply.github.com>
2023-05-18 16:24:57 +0300

    next lessons

--- app/src/main/java/com/example/chatapp/activities/SignIn.kt
+++ app/src/main/java/com/example/chatapp/activities/SignIn.kt
@@ -2,15 +2,32 @@ package com.example.chatapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatapp.databinding.ActivitySigininBinding
+import com.example.chatapp.utilities.Constants
+import com.example.chatapp.utilities.PreferenceManager
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+
 
 class SignIn : AppCompatActivity() {
     lateinit var binding: ActivitySigininBinding
+    var preferenceManager: PreferenceManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigininBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preferenceManager = PreferenceManager(applicationContext)
+        if (preferenceManager!!.getBoolean(Constants.KEY_IS_SIGNED_IN)!!) {
+            val intent = Intent(applicationContext, MainChat::class.java)
+            startActivity(intent)
+            finish()
+        }
         setListeners()
     }
 
@@ -18,14 +35,74 @@ class SignIn : AppCompatActivity() {
     {
         binding.btnSignIn.setOnClickListener()
         {
-
+            if (isValidSignIn() == true)  SignIn()
         }
         binding!!.txtCreateNewAccount.setOnClickListener()
         {
             startActivities(arrayOf(Intent(this,Sing_Up::class.java)))
         }
     }
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
 
+    private fun isValidSignIn(): Boolean? {
+        if (binding.inputEmail.text.toString().trim().isEmpty()) {
+            showToast("Enter the Email")
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.text.toString().trim())
+                .matches()
+        ) {
+            showToast("Enter valid Email")
+            return false
+        } else if (binding.inputPassword.text.toString().trim().isEmpty()) {
+            showToast("Enter the Password")
+            return false
+        }
+        return true
+    }
+
+    private fun SignIn() {
+        loading(true)
+        val db = FirebaseFirestore.getInstance()
+        db.collection(Constants.KEY_COLLECTION_USERS)
+            .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.text.toString().trim())
+            .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.text.toString().trim())
+            .get()
+            .addOnCompleteListener(OnCompleteListener { task: Task<QuerySnapshot?> ->
+                if (task.isSuccessful && task.result != null && task.result!!
+                        .documents.size > 0
+                ) {
+                    val snapshot = task.result!!.documents[0]
+                    preferenceManager!!.putBoolean(Constants.KEY_IS_SIGNED_IN, true)
+                    preferenceManager!!.setString(Constants.KEY_USER_ID, snapshot.id)
+                    preferenceManager!!.setString(
+                        Constants.KEY_NAME,
+                        snapshot.getString(Constants.KEY_NAME)
+                    )
+                    preferenceManager!!.setString(
+                        Constants.KEY_IMAGE,
+                        snapshot.getString(Constants.KEY_IMAGE)
+                    )
+                    val intent = Intent(applicationContext, MainChat::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
+                } else {
+                    loading(false)
+                    showToast("Unable to Sign In")
+                }
+            })
+    }
+
+    private fun loading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.btnSignIn.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.btnSignIn.visibility = View.VISIBLE
+        }
+    }
     //Test Connection Database
     /*private fun addDataToFirestore() {
         val dataBase = FirebaseFirestore.getInstance()
--- app/src/main/java/com/example/chatapp/utilities/Constants.kt
+++ app/src/main/java/com/example/chatapp/utilities/Constants.kt
@@ -10,5 +10,37 @@ class Constants {
         var KEY_IS_MAIN_CHAT = "isMainChat"
         var KEY_USER_ID = "userId"
         var KEY_IMAGE = "image"
+
+        var KEY_IS_SIGNED_IN = "isSingedIn"
+        var KEY_FCM_TOKEN = "fcmToken"
+        var KEY_USER = "user"
+        var KEY_COLLECTION_CHAT = "chat"
+        var KEY_SENDER_ID = "senderId"
+        var KEY_RECEIVER_ID = "receierId"
+        var KEY_MESSAGE = "message"
+        var KEY_TIMESTAMP = "timeStamp"
+        var KEY_COLLECTION_CONVERSATION = "conversations"
+        var KEY_SENDER_NAME = "senderName"
+        var KEY_RECEIVER_NAME = "receiverName"
+        var KEY_SENDER_IMAGE = "sanderImg"
+        var KEY_RECEIVER_IMAGE = "receiverImg"
+        var KEY_LAST_MESSAGE = "lastMessage"
+        var KEY_AVAILABLE = "available"
+        var REMOTE_MSG_AUTHORIZATION = "Authorization"
+        var REMOTE_MSG_CONTENT_TYPE = "Content-Type"
+        var REMOTE_MSG_DATA = "data"
+        var REMOTE_MSG_REGISTRATION_IDS = "registration_ids"
+        var remoteMsgHeaders: HashMap<String, String>? = null
+    }
+
+    fun getRemoteMsgHeaders(): HashMap<String, String>? {
+        if (remoteMsgHeaders == null) {
+            remoteMsgHeaders = java.util.HashMap()
+            remoteMsgHeaders!![REMOTE_MSG_AUTHORIZATION] =
+                "key=AAAAXc-oW3Q:APA91bFipeWVuaHgkfAX7KbsWC3_9etZajwD5k0ykUs7xKwULojtPDqVSK7ey7e0ZzdfZg94kPqSlluy6rqQ6lNpoe38xqqBJ64jpdzxULJjScs11_7FYKZvIRkRQaRoUqCX5FoA2UQ_"
+            remoteMsgHeaders!![REMOTE_MSG_CONTENT_TYPE] =
+                "application/json"
+        }
+        return remoteMsgHeaders
     }
 }
